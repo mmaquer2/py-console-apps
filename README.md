d90719515749dc3b20236436e9b30db7ef197be9
mike maquera <mike.maquera212@gmail.com>
2020-08-25 14:00:35 -0500

    updated pandas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # py-console-apps
+
+- microsoft office based terminal line productivity apps
+
+- modules used python-docx, pandas, openpyxl
--- createSheet.py
+++ createSheet.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import openpyxl as openpyxl
+
+
+#name of workbook
+#name of rows
+
+
+#import data from where?
+
+# create a filename?
